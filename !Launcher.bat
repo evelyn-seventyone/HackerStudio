60e78f007d72765a5869a7d8c0b1934a1ceb66b3
evelyn71 <61363024+evelyn-seventyone@users.noreply.github.com>
2020-05-08 14:38:18 +0300

    Add files via upload

---
+++ !Launcher.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 65001
+title HS ModLoader Launcher
+echo Лаунчер HS ModLoader 2.6-pre
+echo Модификация лаунчера by evelyn-seventyone
+echo Используйте [dir] для просмотра модов
+cmd /k "cd C:\HS Files\mods"
